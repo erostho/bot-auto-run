da89a626827ab2700b78729bb3835cb41930f6e1
erostho <erostho1202@gmail.com>
2025-06-30 17:12:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,7 @@ from gspread_dataframe import set_with_dataframe
 # ======================================
 def connect_sheet(sheet_name, worksheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    import os, json
-    creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name)
     try:
@@ -159,9 +157,7 @@ def run_full_analysis():
 
     # KẾT NỐI SHEET
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    import os, json
-    creds_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name)
 
