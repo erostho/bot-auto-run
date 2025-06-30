f3e9eb9eb574cf98708f8b41ad1c5d2a47dd7844
erostho <erostho1202@gmail.com>
2025-06-30 16:34:19 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from gspread_dataframe import set_with_dataframe
 # ======================================
 def connect_sheet(sheet_name, worksheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name)
     try:
